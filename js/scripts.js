781d7385af80241ba6863ef8f9ed386ef9019bdc
todeschi <adrien.todeschini@inria.fr>
2016-02-24 03:53:06 +0100

    body black

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@
 
 var n = 3;
 var mycube = newCube(n);
-mycube = flipCube(mycube, 0, n-1);
-mycube = flipCube(mycube, 1, n-1, false);
-mycube = flipNCube(mycube, 2, 0, true, 2);
+//mycube = flipCube(mycube, 0, n-1);
+//mycube = flipCube(mycube, 1, n-1, false);
+//mycube = flipNCube(mycube, 2, 0, true, 2);
 
 var colors = {U:'#FFF', L:'#0F0', F:'#F00', R:'#00F', B:'#F0F', D:'#FF0'};
 document.getElementById('cube').innerHTML = printCube(mycube, colors);
