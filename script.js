92c6ea3d68ecf8343a540f15fda5cb1add66492e
todeschi <adrien.todeschini@inria.fr>
2016-02-27 00:58:00 +0100

    keyboard controls

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ var myIds = Cube.ids('my-cube', 'my-moves', 'my-nshuf', 'my-txtmov');
 
 myCube.refresh(myIds.cube, myIds.moves);
 document.getElementById('my-ctrl').innerHTML = Cube.movesCtrlHTML(varname, myIds) + Cube.playCtrlHTML(varname, myIds);
+
+var moveKeys = {
+    66: "B",
+    70: "F",
+    82: "R",
+    76: "L",
+    68: "D",
+    85: "U"
+}
+
+document.addEventListener('keydown', function(e) {
+    var key = moveKeys[e.keyCode];
+    if (key != undefined) {
+        if (e.shiftKey)
+            key += e.ctrlKey ? "2" : "'";
+        else if (e.ctrlKey)
+            return false;
+        myCube.move(key).refresh('my-cube', 'my-moves');
+    }
+    return false;
+});
